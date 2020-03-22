3c062f32955ee3cd819c034252fd35c58be94797
mbalcer <mateusz75319@gmail.com>
2020-03-22 22:59:44 +0100

    Created basic view

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {AfterViewChecked, Component} from '@angular/core';
+
+declare var showMap: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked {
 
+  constructor() {
+
+  }
+
+  ngAfterViewChecked(): void {
+    new showMap();
+  }
 }
